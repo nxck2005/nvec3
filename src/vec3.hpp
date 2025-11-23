2bf2ba9b89b0dd6197eaf8e7f188554e4f6689c2
Nishchal Ravi <nxckmusicprod@gmail.com>
2025-11-23 18:26:37 +0530

    Add dot cross and length funcs

--- src/vec3.hpp
+++ src/vec3.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 struct Vec3 {
     float x;
     float y;
@@ -65,8 +66,29 @@ struct Vec3 {
     static Vec3 up() {
         return Vec3(0.0f, 1.0f, 0.0f);
     }
+
+    // length func
+    float length() const {
+        return std::sqrt((this->x * this->x) + (this->y * this->y) + (this->z + this->z));
+    }
+    // length^2 func
+    float length2() const {
+        return (this->x * this->x) + (this->y * this->y) + (this->z + this->z);
+    }
     // add linear interp later
 
+    // static funcs
+    // anything that treats both vecs equally are going to be implemented as static.
     // Vector and Dot product
+    static float dot(const Vec3& first, const Vec3& second) {
+        return (first.x * second.x) + (first.y * second.y) + (first.z * second.z);
+    }
 
+    static Vec3 cross(const Vec3& first, const Vec3& second) {
+        return Vec3(
+            (first.y * second.z) - (first.z * second.y),  // x component
+            (first.z * second.x) - (first.x * second.z),  // y component
+            (first.x * second.y) - (first.y * second.x)   // z component
+        );
+    }
 };
